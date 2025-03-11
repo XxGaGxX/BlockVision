33107d2db75de4a64bf6dcf56878ae62d7f0685a
diego vagnini <diego.vagnini6@gmail.com>
2025-03-11 23:30:38 +0100

    Aggiunta del componente HomePage con animazione ScrambleHover e stili CSS. Aggiornamento delle dipendenze nel package.json.

--- frontend/MoneyMateFrontend/src/components/HomePage.jsx
+++ frontend/MoneyMateFrontend/src/components/HomePage.jsx
@@ -1,7 +1,22 @@
 import "./HomePage.css"
+import ScrambleHover from "../animations/scramble"
 
 function HomePage(){
-    return <h1>HomePage</h1>
+    return (
+        <div className="HomePageDiv1">
+            <ScrambleHover
+                text={'il mio nome e diego vagnini'}
+                scrambleSpeed={40}
+                sequential={true}
+                revealDirection="start"
+                useOriginalCharsOnly={false}
+                className="font-azeretMono"
+                characters="abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+-=[]{}|;':\,./<>?"
+            />
+        </div>
+        
+    )
+   
 }
 
 export default HomePage
