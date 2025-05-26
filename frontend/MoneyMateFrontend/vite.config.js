ba45283dec63060a6cf3cf6702c464f2011d76e9
diego <diego.vagnini6@gmail.com>
2025-05-26 20:11:36 +0200

    style: format vite.config.js for improved readability

--- frontend/MoneyMateFrontend/vite.config.js
+++ frontend/MoneyMateFrontend/vite.config.js
@@ -5,7 +5,8 @@ import tailwind from "tailwindcss"
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+  plugins: [react()],
+  base: process.env.VITE_BASE_PATH || "/BlockVision/frontend/MoneyMateFrontend",
   server: {
     open : true
   }
